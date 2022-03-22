6fd91738bae78ce6f7ad983966a4ba977ce11ede
Tu Mai Anh Do <tudo@usc.edu>
2022-03-22 14:17:50 -0700

    one in situ step

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -35,7 +35,10 @@ namespace wrench {
                            const std::vector<std::shared_ptr<SimpleStorageService>> &storage_services,
                            const std::string &hostname) :
             ExecutionController(hostname,"controller"),
-            compute_services(compute_services), storage_services(storage_services) {}
+            compute_services(compute_services), storage_services(storage_services) {
+        WRENCH_INFO("Number of compute services: %d", this->compute_services.size());
+        WRENCH_INFO("Number of storage services: %d", this->storage_services.size());
+    }
 
     /**
      * @brief main method of the Controller
@@ -51,31 +54,34 @@ namespace wrench {
         WRENCH_INFO("Controller starting");
 
         /* Input parameters */
-        int num_simulations = 1;
-        std::vector<int> num_analyses_per_simulation = {1};
-        std::vector<std::tuple<int, int>> cosched_allocations{{0,1}};
-        int num_nodes = 2;
+        // conint num_simulations = 1;
+        // std::vector<int> num_analyses_per_simulation = {1};
+        std::vector<std::vector<int>> allocation_map = {{0,0,1,2}}; 
+        std::vector<std::tuple<int, int>> cosched_allocations = {{0,1},{2,2},{3,3}};
+        // int num_nodes = 2;
         double data_size = 1 * GBYTE;
         double compute_flops = 100 * GFLOP;
         double compute_mem = 50 * MBYTE;
         double analysis_flops = 100 * GFLOP;
         double analysis_mem = 50 * MBYTE;
 
-        
-
         /* Create a job manager so that we can create/submit jobs */
         auto job_manager = this->createJobManager();
 
-        /* Instantiate a batch compute service, and add it to the simulation.
-         * This means that actions running on this service will access data files only from remote storage services. */
-        std::vector<std::shared_ptr<wrench::CompoundJob>> jobs;
+        int num_simulations = allocation_map.size();
+        int num_jobs = 0;
+
+        /* Define fine-grained stages for ensemble members */
         for (int i = 1; i <= num_simulations; i++) {
-            int simulation_allocation = 0;
+            int simulation_allocation = allocation_map[i-1][0];
             std::vector<std::shared_ptr<wrench::CompoundJob>> data_write_jobs;
+
             int simulation_node_start = std::get<0>(cosched_allocations[simulation_allocation]);
             int simulation_node_end = std::get<1>(cosched_allocations[simulation_allocation]);
             int simulation_num_nodes = simulation_node_end - simulation_node_start + 1;
             double simulation_data_size = data_size / simulation_num_nodes;
+
+            WRENCH_INFO("Simulation %d is co-scheduled on co-scheduling allocation %d from node %d to node %d, each node writes %.2lf bytes", i, simulation_allocation, simulation_node_start, simulation_node_end, simulation_data_size);
             for (int node = simulation_node_start; node <= simulation_node_end; node++) {
                 auto simulation_storage = this->storage_services[node];
                 auto simulation_compute = this->compute_services[node];
@@ -93,37 +99,45 @@ namespace wrench {
                 data_write_job->addParentJob(compute_job);
                 job_manager->submitJob(data_write_job, simulation_compute);
                 data_write_jobs.push_back(data_write_job);
-            }
-
-            for (int j = 1; j <= num_analyses_per_simulation[i-1]; j++) {
                 
-                int analysis_allocation = 0;
+                num_jobs += 2;
+            }
+            std::vector<std::shared_ptr<wrench::CompoundJob>> data_read_jobs;
+            for (int j = 1; j <= (allocation_map[i-1]).size()-1; j++) {
+                
+                int analysis_allocation = allocation_map[i-1][j];
                 int analysis_node_start = std::get<0>(cosched_allocations[analysis_allocation]);
                 int analysis_node_end = std::get<1>(cosched_allocations[analysis_allocation]);
                 int analysis_num_nodes = (analysis_node_end - analysis_node_start + 1);
                 double analysis_data_size = data_size / analysis_num_nodes;
+
                 int analysis_storage_node = simulation_node_start;
+                WRENCH_INFO("Analysis %d (simulation %d) is co-scheduled on co-scheduling allocation %d from node %d to node %d, each node reads %.2lf bytes", j, i, analysis_allocation, analysis_node_start, analysis_node_end, analysis_data_size);
                 for (int node = analysis_node_start; node <= analysis_node_end; node++) {
+                    if (analysis_storage_node > simulation_node_end) 
+                        analysis_storage_node = simulation_node_start; 
                     auto analysis_storage = this->storage_services[analysis_storage_node];
                     auto analysis_compute = this->compute_services[node];
 
                     /* Reading stage */
-                    auto input_data = wrench::Simulation::addFile("member_" + std::to_string(i) + "_input_data_" + std::to_string(node), analysis_data_size);
+                    auto input_data = wrench::Simulation::addFile("member_" + std::to_string(i) + "_" + std::to_string(j) + "_input_data_" + std::to_string(node), analysis_data_size);
                     analysis_storage->createFile(input_data, wrench::FileLocation::LOCATION(analysis_storage));
-                    auto data_read_job = job_manager->createCompoundJob("member_" + std::to_string(i) + "_data_read_job_" + std::to_string(node));
+                    auto data_read_job = job_manager->createCompoundJob("member_" + std::to_string(i) + "_" + std::to_string(j) + "_data_read_job_" + std::to_string(node));
                     data_read_job->addFileReadAction("data_read", input_data, wrench::FileLocation::LOCATION(analysis_storage));
                     for (auto data_write_job : data_write_jobs) {
                         data_read_job->addParentJob(data_write_job);
                     }
+                    data_read_jobs.push_back(data_read_job);
                     job_manager->submitJob(data_read_job, analysis_compute);
 
                     /* Analyzing stage */
-                    auto analysis_job = job_manager->createCompoundJob("member_" + std::to_string(i) + "_analysis_job_" + std::to_string(node));
+                    auto analysis_job = job_manager->createCompoundJob("member_" + std::to_string(i) + "_" + std::to_string(j) + "_analysis_job_" + std::to_string(node));
                     analysis_job->addComputeAction("analysis", analysis_flops, analysis_mem, 1, 3, wrench::ParallelModel::AMDAHL(0.8));
                     analysis_job->addParentJob(data_read_job);
                     job_manager->submitJob(analysis_job, analysis_compute);
 
                     analysis_storage_node++;
+                    num_jobs += 2;
                 }
             }
             
@@ -136,8 +150,8 @@ namespace wrench {
         //     WRENCH_INFO("Submitting job %s to the bare-metal compute service", jobs[i-1]->getName().c_str());
         // }
 
-        for (int i = 1; i <= 8; i++) {
-            WRENCH_INFO("Waiting for an execution event...");
+        for (int i = 1; i <= num_jobs; i++) {
+            // WRENCH_INFO("Waiting for an execution event...");
             this->waitForAndProcessNextEvent();
         }
 
@@ -153,23 +167,24 @@ namespace wrench {
      * @param event: the event
      */
     void Controller::processEventCompoundJobCompletion(std::shared_ptr<CompoundJobCompletedEvent> event) {
+        TerminalOutput::setThisProcessLoggingColor(TerminalOutput::COLOR_BLUE);
         /* Retrieve the job that this event is for */
         auto job = event->job;
         /* Print info about all actions in the job */
-        WRENCH_INFO("Notified that compound job %s has completed:", job->getName().c_str());
-        WRENCH_INFO("It had %lu actions:", job->getActions().size());
+        // WRENCH_INFO("It had %lu actions:", job->getActions().size());
         for (auto const &action : job->getActions()) {
-            WRENCH_INFO("  - Action %s ran on host %s (physical: %s)",
+            WRENCH_INFO("  - Compound job %s completed: Action %s ran on host %s (physical: %s), used %lu cores for computation, and %.2lf bytes of RAM, started at time %.2lf and finished at time %.2lf",
+                        job->getName().c_str(),
                         action->getName().c_str(),
                         action->getExecutionHistory().top().execution_host.c_str(),
-                        action->getExecutionHistory().top().physical_execution_host.c_str());
-            WRENCH_INFO("     - it used %lu cores for computation, and %.2lf bytes of RAM",
+                        action->getExecutionHistory().top().physical_execution_host.c_str(),
                         action->getExecutionHistory().top().num_cores_allocated,
-                        action->getExecutionHistory().top().ram_allocated);
-            WRENCH_INFO("     - it started at time %.2lf and finished at time %.2lf",
+                        action->getExecutionHistory().top().ram_allocated,
                         action->getExecutionHistory().top().start_date,
-                        action->getExecutionHistory().top().end_date);
+                        action->getExecutionHistory().top().end_date
+                        );
         }
+        TerminalOutput::setThisProcessLoggingColor(TerminalOutput::COLOR_GREEN);
     }
 
     /**
